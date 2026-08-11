0ab9ee389006167eb758678b6df630452b7f8def
kimbob56kr6 <kimbob56kr6@gmail.com>
2026-08-11 09:14:40 -0400

    Change port to use environment variable
    Updated the application to run on a dynamic port from environment variables.

--- main.py
+++ main.py
@@ -65,5 +65,5 @@ def make_kakao_response(text):
         }
     })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
